d8190d9b7cd87ca3fd780a36c3beb5add7897f18
Hendi Santika <hendisantika@gmail.com>
2020-01-14 06:30:27 +0700

    #3 - Add download file service

--- src/app/file.service.ts
+++ src/app/file.service.ts
@@ -1,10 +1,17 @@
 import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpResponse} from '@angular/common/http';
+import {Http, ResponseContentType} from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileService {
 
-  constructor() {
+  constructor(private http: Http) {
+  }
+
+  downloadFile(): Observable<HttpResponse<Blob>> {
+    return this.http.get('http://localhost:8080/employees/download', {responseType: ResponseContentType.Blob});
   }
 }
